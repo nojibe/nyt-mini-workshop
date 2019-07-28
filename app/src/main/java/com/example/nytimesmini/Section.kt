7ccf7b54c3d2105b363cbb7370b823082a11b36c
Nnamdi Ojibe <chibizoar@gmail.com>
2019-07-28 11:07:02 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/nytimesmini/Section.kt
@@ -0,0 +1,20 @@
+package com.example.nytimesmini
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Section(val results: List<Result>)
+
+@JsonClass(generateAdapter = true)
+data class Result(
+    val title: String,
+    val abstract: String,
+    val url: String,
+    val multimedia: List<Multimedia>
+)
+
+@JsonClass(generateAdapter = true)
+data class Multimedia(
+    val url: String,
+    val format: String
+)
